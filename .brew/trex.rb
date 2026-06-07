4f965958c6ce874cd969e260c5dcbb25c21cb5fd
cristiano-s-r-filho <cristiano.sr.filho@proton.me>
2026-06-07 11:41:16 -0300

    update .brew/trex.rb sha256, .scoop/trex.json sha256

--- .brew/trex.rb
+++ .brew/trex.rb
@@ -2,7 +2,7 @@ class Trex < Formula
   desc "Terminal-based collaborative text editor with P2P sync and post-quantum security"
   homepage "https://github.com/cristiano-s-r-filho/telarex"
   url "https://github.com/cristiano-s-r-filho/telarex/archive/refs/tags/v0.5.0-beta.tar.gz"
-  sha256 "0000000000000000000000000000000000000000000000000000000000000000"
+  sha256 "9818769f171bee29b5fbc3a9608d1edf054192a068ec21edd60721c7bc675c31"
   license "MIT OR Apache-2.0"
 
   depends_on "rust" => :build
